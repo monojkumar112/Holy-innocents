ebaad67d43e701fc1e95d2f0c1044665efc228b1
MH Shakil <mehadi.womenindigital@gmail.com>
2025-10-14 18:01:20 +0600

    Implement dynamic newsletter fetching in NewsLetter component; update rendering logic for title, description, and image.

--- app/components/pages/home/NewsLetter.jsx
+++ app/components/pages/home/NewsLetter.jsx
@@ -1,7 +1,22 @@
+'use client'
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const NewsLetter = () => {
+  // fetch newslatter form api 
+  const [newsletter, setNewsletter] = useState(null);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const fetchNewsletter = async () => {
+    const response = await fetch(`${baseUrl}/api/newsletter`);
+    const data = await response.json();
+    setNewsletter(data.data);
+  };
+  React.useEffect(() => {
+    if (!baseUrl) return;
+    fetchNewsletter();
+  }, [baseUrl]);
+  console.log("Newsletter", newsletter);
+
   return (
     <>
       <section
@@ -12,13 +27,16 @@ const NewsLetter = () => {
         <div className="container">
           <div className="newsletter-wrapper">
             <div className="newsletter-left">
-              <h2 className="section-title">Current Newsletter</h2>
-              <p>
-                Stay updated with the latest news and events from our parish
-                community.
-              </p>
+              <h2 className="section-title">{newsletter?.title ? newsletter.title : "Current Newsletter"}</h2>
+
+              <div dangerouslySetInnerHTML={{
+                __html: newsletter?.description
+                  ? newsletter.description
+                  : "Stay updated with the latest news and events from our parish community.",
+              }} />
+
               <div className="mass-event-btn-group">
-                <a href="/about.html" className="custom-btn download-btn">
+                <a target="_blank" href={newsletter?.file_link ? newsletter.file_link : "#"} className="custom-btn download-btn">
                   Download
                 </a>
                 <a href="/#contact" className="custom-btn-alt subscribe-btn">
@@ -28,7 +46,7 @@ const NewsLetter = () => {
             </div>
             <div className="newsletter-right">
               <Image
-                src={"/assets/images/news.png"}
+                src={newsletter?.image ? newsletter.image : "/assets/images/news.png"}
                 width={290}
                 height={270}
                 alt="Newsletter"
