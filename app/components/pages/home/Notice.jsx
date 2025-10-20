80d21a45d2094e81f7681d419e7ecf58f985e5ae
MH Shakil <mehadi.womenindigital@gmail.com>
2025-10-20 12:59:23 +0600

    Implement dynamic fetching of notices in Notice component; add loading state and pagination for enhanced user experience.

--- app/components/pages/home/Notice.jsx
+++ app/components/pages/home/Notice.jsx
@@ -1,6 +1,57 @@
-import React from "react";
+'use client'
+import React, { useState, useEffect, useCallback } from "react";
 
 const Notice = () => {
+
+  const [notices, setNotices] = useState([]);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const [nextPageUrl, setNextPageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
+
+
+
+
+  const fetchNotices = async () => {
+
+    try {
+      setLoading(true);
+      const response = await fetch(`${baseUrl}/api/notices`);
+      const notice = await response.json();
+      setNotices(notice.data.data);
+      setNextPageUrl(notice.data.next_page_url);
+    } catch (error) {
+      console.error('Error fetching notices:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadMore = useCallback(async () => {
+    if (!nextPageUrl || loadingMore) return;
+
+    setLoadingMore(true);
+    try {
+      const response = await fetch(nextPageUrl);
+      const data = await response.json();
+      setNotices(prevNotices => [...prevNotices, ...data.data.data]);
+      setNextPageUrl(data.data.next_page_url);
+      console.log('next notice', data.data.data)
+    } catch (error) {
+      console.error('Error loading more notices:', error);
+    } finally {
+      setLoadingMore(false);
+    }
+  }, [nextPageUrl, loadingMore]);
+
+  useEffect(() => {
+    fetchNotices();
+  }, []);
+
+
+  // console.log('Notice', notices);
+
   return (
     <>
       <section
@@ -10,45 +61,43 @@ const Notice = () => {
       >
         <div className="container">
           <div className="row">
-            <div className="col-md-6 col-lg-6">
-              <div className="notice-left">
-                <h2>Parish notice</h2>
-                <div className="parish-content">
-                  <p>
-                    Youth Ministry began 
-                    <br />
-                    9th May 2025
-                  </p>
-                  <p>
-                    <span>Catholic Bishops of England</span>, Wales, and
-                    Scotland:
-                    <br />
-                    ​Statement concerning the Bill on assisted suicide here
-                  </p>
-                  <p>
-                    Zoom meetings for services, prayer times and children&apos;
-                    s rosary here 
-                  </p>
+
+            {notices.map((notice) => (
+              <div className="col-md-6 col-lg-6 mb-3" key={notice.id}>
+                <div
+                  className="notice-left"
+                  style={{ backgroundColor: notice.card_color || '#ffffff' }}
+                >
+                  <h2>{notice.title ? notice.title : 'No Title Available'}</h2>
+                  <div className="parish-content">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: notice?.body ?? 'No Content Available',
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-6 col-lg-6">
-              <div className="notice-left notice-other">
-                <h2>Other Notices</h2>
-                <div className="parish-content">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ipsam nemo obcaecati dolorem esse temporibus aliquam ex
-                    voluptates pariatur maiores atque!
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ipsam nemo obcaecati dolorem esse temporibus aliquam ex
-                    voluptates pariatur maiores atque!
-                  </p>
-                </div>
+            ))}
+
+          </div>
+          <div>
+            {/* see more  */}
+            {nextPageUrl && (
+              <div className="d-flex see-more-btn justify-content-center">
+                <button
+                  onClick={loadMore}
+                  disabled={loadingMore}
+                  className="custom-btn load-more-btn btn-sm"
+                  style={{
+                    cursor: loadingMore ? 'not-allowed' : 'pointer',
+                    opacity: loadingMore ? 0.6 : 1
+                  }}
+                >
+                  {loadingMore ? 'Loading...' : 'See More'}
+                </button>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </section>
