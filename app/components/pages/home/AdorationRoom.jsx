adcb21058a49a18ff7daf9c4ef9cd58e71a4060c
monojkumar2001 <monojkumar57859893@gmail.com>
2025-11-04 18:05:48 +0600

    update

--- app/components/pages/home/AdorationRoom.jsx
+++ app/components/pages/home/AdorationRoom.jsx
@@ -11,7 +11,7 @@ const AdorationRoom = () => {
       >
         <div className="container">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-5">
               <div className="adoration-img">
                 <Image
                   src={"/assets/images/charis.png"}
@@ -21,12 +21,12 @@ const AdorationRoom = () => {
                 />
               </div>
             </div>
-            <div className="col-md-8">
+            <div className="col-md-7">
               <div className="adoration-content">
-                <h2>Visit our St. Carlo Acutis Adoration Room</h2>
+                <h2>Visit Our Saint Carlo Acutis’ Adoration Room</h2>
                 <h5>
-                  A sacred place for prayer, reflection, and spiritual
-                  connection.
+                  A sacred quiet and peaceful place for Prayer, Reflection, and
+                  Spiritual Connection
                 </h5>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
