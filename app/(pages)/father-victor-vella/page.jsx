52d313664e08cf0ff499b029c9c654fd4c69e24f
monojkumar2001 <monojkumar57859893@gmail.com>
2025-11-14 13:37:48 +0600

    update

--- app/(pages)/father-victor-vella/page.jsx
+++ app/(pages)/father-victor-vella/page.jsx
@@ -28,7 +28,7 @@ const fatherVictorVellapage = () => {
           >
             <div className="father-img">
               <Image
-                src="/assets/images/father.png  "
+                src="/assets/images/father.png "
                 alt="Father Victor Vella"
                 width={1250}
                 height={743}
